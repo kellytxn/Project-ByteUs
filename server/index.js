fad54c977a477fa168d66c16d03b6a3f1a56e17b
ShangYiQian <syq5678@gmail.com>
2025-05-22 03:38:48 +0800

    app imports

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.json({message: "Hello from the server"});
 });
