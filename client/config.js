a7f8659676175e85d538e4d6941a5d5d355a4bb0
kel <kellytan@kellys-MacBook-Pro.local>
2025-05-28 17:53:32 +0800

    render url as backend url

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://192.168.1.109:5001";
-//export const BACKEND_URL = "https://project-byteus.onrender.com";
+//export const BACKEND_URL = "http://192.168.1.109:5001";
+export const BACKEND_URL = "https://project-byteus.onrender.com";
