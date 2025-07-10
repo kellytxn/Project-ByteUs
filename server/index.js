62f48b34b9fe1272e2a4bc592025929d061e6e94
kel <kellytan@kellys-MacBook-Pro.local>
2025-07-10 14:09:53 +0800

    file too large error

--- server/index.js
+++ server/index.js
@@ -25,8 +25,8 @@ const User = mongoose.model("UserInfo");
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
