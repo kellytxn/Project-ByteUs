2352b7291bba7b70d15b192f6d1f7215ae910784
yi qian <syq5678@gmail.com>
2025-08-09 18:37:31 +0800

    swapped backend

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 //yq's//
-export const BACKEND_URL = "http://192.168.10.145:8000";
+//export const BACKEND_URL = "http://192.168.10.145:8000";
 //kelly's
 //export const BACKEND_URL = "http://192.168.1.109:8000";
-//export const BACKEND_URL = "https://project-byteus.onrender.com";
+export const BACKEND_URL = "https://project-byteus.onrender.com";
